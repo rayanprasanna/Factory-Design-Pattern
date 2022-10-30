98c6d954e283ff035b7607430df01a917a4e4be3
rayanprasanna <rayanprasanna224@gmail.com>
2022-10-30 05:49:15 +0530

    Factory Design Pattern

---
+++ src/NotificationFactory.java
@@ -0,0 +1,13 @@
+public class NotificationFactory {
+    public Notification createNotification(String channel)
+    {
+        if (channel == null || channel.isEmpty())
+            return null;
+        return switch (channel) {
+            case "SMS" -> new SMSNotification();
+            case "EMAIL" -> new EmailNotification();
+            case "PUSH" -> new PushNotification();
+            default -> throw new IllegalArgumentException("Unknown channel " + channel);
+        };
+    }
+}
---
+++ src/NotificationService.java
@@ -0,0 +1,8 @@
+public class NotificationService {
+    public static void main(String[] args)
+    {
+        NotificationFactory notificationFactory = new NotificationFactory();
+        Notification notification = notificationFactory.createNotification("SMS");
+        notification.notifyUser();
+    }
+}
